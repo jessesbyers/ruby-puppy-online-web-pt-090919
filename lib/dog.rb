590269f8a370e24c57e48a4a322525752f0dd32e
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-23 16:57:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
+  attr_reader :name
   @@all = []
-
 
   def initialize(name)
     @name = name
@@ -9,6 +9,12 @@ class Dog
 
   def self.all
     @@all
+  end
+
+  def self.clear_all
+  end
+
+  def self.print_all
   end
 
 
