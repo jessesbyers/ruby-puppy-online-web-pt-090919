26eefa39b50d53e11d6211844e1a64ddc85e6a04
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-23 16:50:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,15 @@
 class Dog
 
+attr_reader :dog_array
 
   def initialize(name)
     @name = name
+    dog_array = []
+    dog_array << name
   end
 
   def self.all
-    
+    dog_array
   end
 
 end
